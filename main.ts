3725062d16ddeee95aa62f0c83fd16d291350cf8
karinashai <115450507+karinashai@users.noreply.github.com>
2022-10-10 14:49:57 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.showNumber(0)
+basic.showNumber(1)
+basic.showNumber(2)
+basic.showNumber(3)
+basic.showNumber(4)
+basic.showNumber(5)
+basic.showNumber(6)
+basic.forever(function () {
+	
+})
